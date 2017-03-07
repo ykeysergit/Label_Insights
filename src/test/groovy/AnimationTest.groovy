bfc3056feb7b9361268238dfff32db88d8b645e5
ykeysergit <tangeroknight@gmail.com>
2017-03-07 03:32:38 -0600

    update

--- src/test/groovy/AnimationTest.groovy
+++ src/test/groovy/AnimationTest.groovy
@@ -8,11 +8,19 @@ class AnimationTest extends Specification{
 		Animation.animate(speed, init) == result
 		
 		where:
-		speed | init     | result
-		2 	  | '...'    | ['...']
-		1	  | 'R..'    | ['X..', '.X.', '..X', '...']
-		1	  | '..L'    | ['..X', '.X.', 'X..', '...']
-		1	  | 'R..L'   | ['X..X', '.XX.', '.XX.', 'X..X','....']
-		2	  | '..R....' | ['..X....', '....X..', '......X','.......']
+		speed | init         		  | result
+		1 	  | '...'        		  | ['...']
+		1	  | 'R..'        		  | ['X..', '.X.', '..X', '...']
+		1	  | '..L'        		  | ['..X', '.X.', 'X..', '...']
+		1	  | 'R..L'       		  | ['X..X', '.XX.', '.XX.', 'X..X','....']
+		2	  | '..R....'    		  | ['..X....', '....X..', '......X','.......']
+		3	  | 'RR..LRL'     		  | ['XX..XXX', '.X.XX..','X.....X', '.......']
+		10	  | 'RLRLRLRLRL' 		  | ['XXXXXXXXXX', '..........']
+		1	  | 'LRRL.LR.LRR.R.LRRL.' | [ "XXXX.XX.XXX.X.XXXX.","..XXX..X..XX.X..XX.",".X.XX.X.X..XX.XX.XX","X.X.XX...X.XXXXX..X",
+										  ".X..XXX...X..XX.X..", "X..X..XX.X.XX.XX.X.", "..X....XX..XX..XX.X", ".X.....XXXX..X..XX.",
+										  "X.....X..XX...X..XX", ".....X..X.XX...X..X", "....X..X...XX...X..", "...X..X.....XX...X.", 
+										  "..X..X.......XX...X", ".X..X.........XX...", "X..X...........XX..", "..X.............XX.", 
+										  ".X...............XX", "X.................X", "..................." 
+									    ]
 	}
 }
